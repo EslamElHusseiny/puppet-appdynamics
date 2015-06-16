179127e70789e228e97a854494316630402ce383
Tom Stockton <tom.stockton@leadx.co.uk>
2015-06-16 12:13:58 +0100

    Add init script.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,5 +1,13 @@
 class appdynamics::service {
 
+  file { "/etc/init.d/appd_machineagent":
+    ensure => file,
+    mode   => 755,
+    owner  => root,
+    group  => root,
+    source => "puppet:///modules/appdynamics/appd_machineagent"
+  }
+  ->
   service { "appd_machineagent":
     ensure => "$appdynamics::service_ensure",
     enable => "$appdynamics::service_enable",
